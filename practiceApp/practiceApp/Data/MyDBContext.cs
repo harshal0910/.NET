521123b6ffc209dea845cb309f437acd285a03d8
7147_harshal <harshalnagtilak2809@gmail.com>
2023-08-13 20:51:09 +0530

    EF

---
+++ practiceApp/practiceApp/Data/MyDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using practiceApp.Models;
+
+namespace practiceApp.Data
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { }   
+
+        public DbSet<Product> products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .Property(p => p.category)
+                .HasConversion(new EnumToStringConverter<Category>());
+        }
+    }
+}
---
+++ practiceApp/practiceApp/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace practiceApp.Models
+{
+    public enum Category{
+        BISCUITS,TOAST,KHARI
+    }
+
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Field is empty")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        [Required(ErrorMessage ="Field is empty")]
+        [EnumDataType(typeof(Category))]
+        public Category category { get; set; }
+        public DateOnly mfgDate { get; set; }
+
+    }
+}
