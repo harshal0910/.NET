e0b3870be5796c8e653e145eaf8cc61257d15b04
7147_harshal <harshalnagtilak2809@gmail.com>
2023-08-15 16:52:57 +0530

    studentApp

---
+++ StudentApp/StudentApp/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
